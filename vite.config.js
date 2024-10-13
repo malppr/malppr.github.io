bd5b3183aefe3edebcc16bf1e20606370c281681
malppr <bryan_chew@mymail.sutd.edu.sg>
2024-10-14 04:55:34 +0800

    bla

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/malppr.github.io/",
+  // base: "/malppr.github.io/",
 });
